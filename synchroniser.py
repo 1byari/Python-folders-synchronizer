1b0665da1ca3b701b3d3ceec2559e9e387df5c69
Oleksandr Popovych <popovole@fit.cvut.cz>
2024-05-06 02:03:36 +0200

    Synchronizer logic improvement

--- synchroniser.py
+++ synchroniser.py
@@ -1,9 +1,10 @@
 import argparse
-import os 
+import hashlib
+import os
 import shutil
 import time
 from datetime import datetime
-import hashlib
+
 
 def parse_arguments():
     """Parse command line arguments for the synchronization script."""
@@ -27,97 +28,126 @@ class Synchronizer:
     def synchronize_folders(self):
         """Perform folder synchronization by removing outdated content and updating necessary files."""
         self.log_message(f"Start of synchronization at {datetime.now()}: ")
-        isRemoveSuccessful = self.remove_content(self.list_files(self.source_path), self.list_files(self.replica_path))
-        isUpdateSuccessful = self.update_content(self.list_files(self.source_path), self.list_files(self.replica_path))
-        if (isRemoveSuccessful == 0 and isUpdateSuccessful == 0):
+        is_remove_successful = self.remove_content(self.list_files(self.replica_path))
+        is_update_successful = self.update_content(self.list_files(self.source_path))
+        if (is_remove_successful == 0 and is_update_successful == 0):
             self.log_message("No changes were necessary.")
-        elif (isRemoveSuccessful != -1 and isUpdateSuccessful != -1):
+        elif (is_remove_successful != -1 and is_update_successful != -1):
             self.log_message("Synchronization completed successfully.")
         else:
             self.log_message("Errors occurred during synchronization.")
 
-    def update_content(self, source_files, replica_files):
+    def update_content(self, source_files):
         """Update or create files and directories from source to replica."""
-        isSuccessful = 0
+        operations_results = []
         for entry in source_files:
             entry_replica_path = entry.replace(self.source_path, self.replica_path)
             # Handle non-existent paths in replica
-            if not entry_replica_path in replica_files:
+            if not os.path.exists(entry_replica_path):
                 if os.path.isdir(entry):
                     # Create directory if it doesn't exist
-                    try:
-                        os.mkdir(entry_replica_path)
-                        os.chmod(entry_replica_path, os.stat(entry).st_mode)
-                        self.log_message(f"Created directory: {entry_replica_path}")
-                        # Recursively update content
-                        res = self.update_content(self.list_files(entry), self.list_files(entry_replica_path))
-                        if res != 0 and isSuccessful != -1:
-                            isSuccessful = res
-                    except Exception as e:
-                        self.log_message(f"Failed to create directory {entry_replica_path}: {e}")
-                        isSuccessful = -1
+                    operations_results.append(self.create_directory(entry, entry_replica_path))
                 else:
                     # Copy file if it doesn't exist
-                    try:
-                        shutil.copy(entry, entry_replica_path)
-                        self.log_message(f"Created file: {entry_replica_path}")
-                        if isSuccessful != -1:
-                            isSuccessful = 1
-                    except Exception as e:
-                        self.log_message(f"Failed to create file {entry_replica_path}: {e}")
-                        isSuccessful = -1
+                    operations_results.append(self.create_file(entry, entry_replica_path))
             elif not os.path.isdir(entry) and not self.compare_files(entry, entry_replica_path):
                 # Update file if changes detected
-                try:
-                    os.remove(entry_replica_path)
-                    shutil.copy2(entry, entry_replica_path)
-                    self.log_message(f"Updated file: {entry_replica_path}")
-                    if isSuccessful != -1:
-                        isSuccessful = 1
-                except Exception as e:
-                    self.log_message(f"Failed to update file {entry_replica_path}: {e}")
-                    isSuccessful = -1
-            else:
-                # Ensure correct permissions are set
-                os.chmod(entry_replica_path, os.stat(entry).st_mode)
-                # Recursively update directories
-                res = self.update_content(self.list_files(entry), self.list_files(entry_replica_path))
-                if res != 0 and isSuccessful != -1:
-                    isSuccessful = res
-        return isSuccessful
+                operations_results.append(self.update_file(entry, entry_replica_path))
+            elif os.path.isdir(entry):
+                # Recursively update directory
+                operations_results.append(self.update_directory(entry, entry_replica_path))
+        return self.calculate_operation_result(operations_results)
+
+    def calculate_operation_result(self, operations_results):
+        """Calculates the result of the operation based on the results of internal operations"""
+        if -1 in operations_results:
+            return -1
+        elif not 1 in operations_results:
+            return 0
+        else:
+            return 1
     
-    def remove_content(self, source_data, replica_data):
+    def update_directory(self, source_dir, replica_dir):
+        """Recursively update the content of the directories."""
+        try:
+            # Ensure correct permissions are set
+            os.chmod(replica_dir, os.stat(source_dir).st_mode)
+            source_files = self.list_files(source_dir)
+            return self.update_content(source_files)
+        except Exception as e:
+            self.log_message(f"Error updating directory {replica_dir}: {e}")
+            return -1
+
+    def update_file(self, source_file, replica_file):
+        try:
+            os.remove(replica_file)
+            shutil.copy2(source_file, replica_file)
+            self.log_message(f"Updated file: {replica_file}")
+            return 1
+        except Exception as e:
+            self.log_message(f"Failed to update file {replica_file}: {e}")
+            return -1
+        
+    def create_file(self, source_file, replica_file):
+        """Creates file identical to source"""
+        try:
+            shutil.copy(source_file, replica_file)
+            self.log_message(f"Created file: {source_file}")
+            return 1
+        except Exception as e:
+            self.log_message(f"Failed to create file {replica_file}: {e}")
+            return -1
+        
+    def create_directory(self, source_dir, replica_dir):
+        """Creates directory identical to source"""
+        try:
+            os.mkdir(replica_dir)
+            os.chmod(replica_dir, os.stat(source_dir).st_mode)
+            self.log_message(f"Created directory: {replica_dir}")
+            # Recursively update content
+            return self.update_directory(source_dir, replica_dir)
+        except Exception as e:
+            self.log_message(f"Failed to create directory {replica_dir}: {e}")
+            return -1
+
+    def remove_content(self, replica_files):
         """Remove files and directories in replica that do not exist in source."""
-        isSuccessful = 0
-        for entry in replica_data:
+        operands_results = []
+        for entry in replica_files:
             entry_source_path = entry.replace(self.replica_path, self.source_path)
             # Check for non-existent source files and remove from replica
-            if not entry_source_path in source_data:
+            if not os.path.exists(entry_source_path):
                 if os.path.isdir(entry):
                     # Remove directory if it doesn't exist in source
-                    try:
-                        shutil.rmtree(entry)
-                        self.log_message(f"Removed directory: {entry}")
-                        isSuccessful = 1
-                    except Exception as e:
-                        self.log_message(f"Failed to remove directory {entry}: {e}")
-                        isSuccessful = -1
+                    operands_results.append(self.remove_directory(entry))
                 else:
                     # Remove file if it doesn't exist in source
-                    try:
-                        os.remove(entry)
-                        self.log_message(f"Removed file: {entry}")
-                        isSuccessful = 1
-                    except Exception as e:
-                        self.log_message(f"Failed to remove file {entry}: {e}")
-                        isSuccessful = -1
+                    operands_results.append(self.remove_file(entry))
             elif os.path.isdir(entry):
                 # Recursively remove content
-                res = self.remove_content(self.list_files(entry), self.list_files(entry)) 
-                if res != 0 and isSuccessful != -1:
-                    isSuccessful = res
-        return isSuccessful
-        
+                operands_results.append(self.remove_content(self.list_files(entry)))  
+        return self.calculate_operation_result(operands_results)
+    
+    def remove_directory(self, directory_path):
+        """Removes directory"""
+        try:
+            shutil.rmtree(directory_path)
+            self.log_message(f"Removed directory: {directory_path}")
+            return 1
+        except Exception as e:
+            self.log_message(f"Failed to remove directory {directory_path}: {e}")
+            return -1
+
+    def remove_file(self, file_path):
+        """Removes file"""
+        try:
+            os.remove(file_path)
+            self.log_message(f"Removed file: {file_path}")
+            return 1
+        except Exception as e:
+            self.log_message(f"Failed to remove file {file_path}: {e}")
+            return -1
+
     def list_files(self, directory_path):
         """List all files in a directory with their absolute paths."""
         abs_directory = os.path.abspath(directory_path)
@@ -146,7 +176,7 @@ class Synchronizer:
                     sha256.update(data)
             return sha256.hexdigest()
         except Exception as e:
-            self.log_message(f"Error reading file {file_path}")
+            self.log_message(f"Error hashing file {file_path}")
 
     def start(self):
         """Start the synchronization process and repeat at the specified time interval."""
